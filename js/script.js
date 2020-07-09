ceb224bd1d7f5ec1bb063c7386d0a59985dabd90
Armin <arminhupka@me.com>
2020-07-09 16:37:56 +0200

    First version

---
+++ js/script.js
@@ -0,0 +1,37 @@
+const mound = document.querySelectorAll('.garden__mound');
+const gardenContainer = document.querySelector('.garden');
+const gardenScore = document.querySelector('.garden__score');
+const startupHeading = document.querySelector('.startup');
+const startBtn = document.querySelector('.startup__play');
+
+let score = 0;
+
+
+const randomMole = () => {
+
+    const mole = document.createElement('img');
+    mole.src = '/assets/mole.svg';
+    let randomNum = 0;
+    randomNum = Math.floor(Math.random() * mound.length);
+    document.getElementById(`${randomNum}`).appendChild(mole).classList.add('garden__mole');
+};
+
+const scoreCounter = () => {
+    gardenScore.textContent = score += 1;
+}
+
+
+startBtn.addEventListener('click', () => {
+    gardenContainer.classList.remove('hide');
+    startupHeading.classList.add('hide');
+    randomMole();
+})
+
+
+gardenContainer.addEventListener('click', (e) => {
+    randomMole();
+    if (e.target.classList.contains('garden__mole')) {
+        e.target.remove();
+        scoreCounter();
+    }
+})
